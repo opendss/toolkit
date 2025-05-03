a596c864a0edd2c7b1dc66ea66837de8befce03f
Qiang Zhao <mattisonchao@apache.org>
2025-05-03 10:07:48 +0800

    feat: support maps

---
+++ pkg/maps/map.go
@@ -0,0 +1,11 @@
+package maps
+
+func ComputeIfAbsent[K comparable, V any](collection map[K]V, key K, fc func(K, V) V) V {
+	v, exist := collection[key]
+	if exist {
+		return v
+	}
+	v = fc(key, v)
+	collection[key] = v
+	return v
+}
